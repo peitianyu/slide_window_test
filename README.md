76469297951a9ac345a12da33671489ebda08414
peitianyu <1216582461@qq.com>
2023-06-07 11:14:07 +0800

    添加边缘化新测试

--- README.md
+++ README.md
@@ -1,26 +1,7 @@
-## 测试模板
+## 参考网址
 
-```shell
-# test
-	- JUST_RUN_TEST
-	- TEST
-	- SKIP_TEST
+[多传感器融合定位理论基础（十二）：滑动窗口原理及应用 - 知乎 (zhihu.com)](https://zhuanlan.zhihu.com/p/150367249)
 
-	- ASSERT_***
-	- EXPECT_***
-# assert
-	- 断言(不过并不会断掉程序)
-# backtrace
-	- 用于查看段错误(已经内置在main函数中)
-# dbg
-	- 用于测试简单的程序
-# log
-	- LOG("HELLO", "WORLD: ", 1) << std::endl;
-	- LOG_FILE(ofs, "HELLO", "WORLD: ", 1) << std::endl;
-```
+## 代码实现了简单的滑窗流程
 
-### !注意
-
-```txt
-1. 使用多个JUST_RUN_TEST时会编译不通过, 会报: multiple definition of `g_justtest_Test'
-```
+[![pCiSonK.jpg](https://s1.ax1x.com/2023/06/06/pCiSonK.jpg)](https://imgse.com/i/pCiSonK)
--- src/test/slide_window_test.cc
+++ src/test/slide_window_test.cc
@@ -68,69 +68,60 @@ void TestSlideWindow()
 
 void TestSlideWindow1()
 {
-    // 初始H与b
-    Eigen::Matrix<double, 6, 6> H;
-    H << 13.7143, 1, 4, 1, 0, 0,
-         1, 1, 1, 1, 0, 0,
-         4, 1, 8, 1, 4, 1,
-         1, 1, 1, 1, 1, 1,
-         0, 0, 4, 1, 8, 1,
-         0, 0, 1, 1, 1, 1;
-    Eigen::Matrix<double, 6, 1> b;
-    b << 4.42857, 6, 5, 6, 2, 3;
+    // Add marg test
+    int idx = 1;            // marg 中间那个变量
+    int dim = 1;            // marg 变量的维度
+    int reserve_size = 3;   // 总共变量的维度
+    int cols = 3;
+    Eigen::MatrixXd H_marg(Eigen::MatrixXd::Zero(cols, cols));
+    H_marg << 1, 2, 7,
+              2, 5, 6,
+              7, 6, 9;
+    std::cout << "---------- TEST Marg: before marg------------"<< std::endl;
+    std::cout << H_marg << std::endl;
 
-    std::cout << "H = \n" << H << std::endl;
-    std::cout << "b = \n" << b.transpose() << std::endl;
+    // TODO:: home work [cg]. 将变量移动到右下角
+    // 将 row i 移动矩阵最下面
+    Eigen::MatrixXd temp_rows = H_marg.block(idx, 0, dim, reserve_size);
+    Eigen::MatrixXd temp_botRows = H_marg.block(idx + dim, 0, reserve_size - idx - dim, reserve_size);
+    H_marg.block(idx, 0, reserve_size - idx - dim, reserve_size) = temp_botRows;
+    H_marg.block(reserve_size - dim, 0, dim, reserve_size) = temp_rows;
 
-    // 1. 使用和要边缘化掉的量无关的因子，构建剩余变量对应的Hessian矩阵和b
-    Eigen::Matrix<double, 4, 4> Hrr_a = H.block<4, 4>(2, 2);
-    Eigen::Matrix<double, 4, 1> br_a = b.tail(4);
+    // 将 col i 移动矩阵最右边
+    Eigen::MatrixXd temp_cols = H_marg.block(0, idx, reserve_size, dim);
+    Eigen::MatrixXd temp_rightCols = H_marg.block(0, idx + dim, reserve_size, reserve_size - idx - dim);
+    H_marg.block(0, idx, reserve_size, reserve_size - idx - dim) = temp_rightCols;
+    H_marg.block(0, reserve_size - dim, reserve_size, dim) = temp_cols;
 
-    std::cout << "-----------------------------" << std::endl;
-    std::cout << "Hrr_a = \n" << Hrr_a << std::endl;
-    std::cout << "br_a = \n" << br_a.transpose() << std::endl;
+    std::cout << "---------- TEST Marg: 将变量移动到右下角------------"<< std::endl;
+    std::cout<< H_marg <<std::endl;
 
-    // 2. 挑出和要边缘化掉的量相关的因子，构建待边缘化的Hessian矩阵，并使用舒尔补做边缘化, 生成先验因子
-    Eigen::Matrix<double, 2, 2> Hrr_b = Hrr_a.block<2, 2>(0, 0);
-    Eigen::Matrix<double, 2, 2> Hrm_b = H.block<2, 2>(2, 0);
-    Eigen::Matrix<double, 2, 2> Hmm_b = H.block<2, 2>(0, 0);
-    Eigen::Matrix<double, 2, 2> Hmr_b = H.block<2, 2>(0, 2);
-    Eigen::Matrix<double, 2, 1> br_b = br_a.head(2);
-    std::cout << "-----------------------------" << std::endl;
-    std::cout << "H_rr_b = \n" << Hrr_b << std::endl;
-    std::cout << "H_rm_b = \n" << Hrm_b << std::endl;
-    std::cout << "H_mm_b = \n" << Hmm_b << std::endl;
-    std::cout << "H_mr_b = \n" << Hmr_b << std::endl;
-    std::cout << "b_r_b = \n" << br_b.transpose() << std::endl;
+    /// 开始 marg ： schur
+    double eps = 1e-8;
+    int m2 = dim;
+    int n2 = reserve_size - dim;   // 剩余变量的维度
+    Eigen::MatrixXd Amm = 0.5 * (H_marg.block(n2, n2, m2, m2) + H_marg.block(n2, n2, m2, m2).transpose());
 
-    Eigen::Matrix<double, 2, 2> H_prior = Hrr_b - Hrm_b * Hmm_b.inverse() * Hmr_b; 
-    Eigen::Matrix<double, 2, 1> b_prior = br_b - Hrm_b * Hmm_b.inverse() * b.head(2);
+    Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> saes(Amm);
+    Eigen::MatrixXd Amm_inv = saes.eigenvectors() * Eigen::VectorXd(
+            (saes.eigenvalues().array() > eps).select(saes.eigenvalues().array().inverse(), 0)).asDiagonal() *
+                              saes.eigenvectors().transpose();
 
-    std::cout << "-----------------------------" << std::endl;
-    std::cout << "H_prior = \n" << H_prior << std::endl;
-    std::cout << "b_prior = \n" << b_prior.transpose() << std::endl;
-    
-    // 3. 结合先验因子，构建待边缘化的Hessian矩阵，并使用舒尔补做边缘化, 生成先验因子
-    Eigen::Matrix<double, 4, 4> Hrr = Hrr_a;
-    Hrr.block<2, 2>(0, 0) += H_prior;
+    // TODO:: home work [cg]. 完成舒尔补操作
+    Eigen::MatrixXd Arm = H_marg.block(0, n2, n2, m2);
+    Eigen::MatrixXd Amr = H_marg.block(n2, 0, m2, n2);
+    Eigen::MatrixXd Arr = H_marg.block(0,  0, n2, n2);
 
-    Eigen::Matrix<double, 4, 1> br = br_a;
-    br.head(2) += b_prior;
+    Eigen::MatrixXd tempB = Arm * Amm_inv;
+    Eigen::MatrixXd H_prior = Arr - tempB * Amr;
 
-    std::cout << "-----------------------------" << std::endl;
-    std::cout << "Hrr = \n" << Hrr << std::endl;
-    std::cout << "br = \n" << br.transpose() << std::endl;
-
-
-    // 结果对比
-    std::cout << "-----------------------------" << std::endl;
-    Eigen::Matrix<double, 6, 1> x = H.inverse() * b;
-    std::cout << "x= \n " << x.transpose() << std::endl;
+    std::cout << "---------- TEST Marg: after marg------------"<< std::endl;
+    std::cout << H_prior << std::endl;
 }
 
 JUST_RUN_TEST(slide_window, test)
 TEST(slide_window, test)
 {
-    TestSlideWindow();
+    // TestSlideWindow();
     TestSlideWindow1();
 }
